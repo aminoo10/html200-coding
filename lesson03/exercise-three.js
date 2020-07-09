d8fe2c069f1d6d03dc11c7838d4f6344efe0f86a
aminoo10 <acminoo95@gmail.com>
2020-07-08 22:05:22 -0700

    added exercise-three.js

---
+++ lesson03/exercise-three.js
@@ -0,0 +1,16 @@
+let things = ['pencil', 'phone', 'scissors', 'towel', 'ruler', 'sketchbook'];
+
+things.forEach(function(thing, index) {
+  console.log(`${index+1},${thing}`);
+  
+  /*wasn't sure whether to start at base 0, but directions said to print out like so:
+  
+        "Example:
+
+        // 1,cats
+        // 2,dogs
+
+        ..and so on"
+    
+   So I did as such */
+});
